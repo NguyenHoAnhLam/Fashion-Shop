6cc4312a9f8c9419ded5a8d27482315d9052f488
NguyenHoAnhLam <nguyenhoanhlam161120000@gmial.com>
2022-05-09 12:41:53 +0700

    Message

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,50 @@ var element = document.querySelector('.category-item-link.active');
 var subElement = document.querySelector('.category__sub')
 element.onclick = function(event){
     event.preventDefault();
-    subElement.classList.add('on__active');
-    element.onclick = function(){
-        subElement.classList.remove('on__active');
-    }
+    subElement.classList.toggle('open');
+    // if(subElement.hasClass('open')){
+    //     subElement.classList.remove('open');
+    // }
 }
+
+const newsLetter = document.querySelector('.js-new-letter');
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-modal-close')
+const modalContainer = document.querySelector('.js-modal-container')
+
+function showLetter(){
+    modal.classList.add('open')
+}
+function removeLetter(){
+    modal.classList.remove('open')
+}
+
+newsLetter.addEventListener('click',showLetter);
+modal.addEventListener('click',removeLetter);
+modalClose.addEventListener('click',removeLetter);
+modalContainer.addEventListener('click',function(e){
+    e.stopPropagation();
+});
+
+const menuMobile = document.querySelector('.mobile__header-menu');
+const navMobile = document.querySelector('.wrapper__sidebar.nav__mobile');
+const closeMobile = document.querySelector('.close__menu-mobile')
+
+function showNavMobile(){
+    navMobile.classList.add('open');
+}
+function removeNavMobile(){
+    navMobile.classList.remove('open');
+}
+menuMobile.addEventListener('click',showNavMobile);
+closeMobile.addEventListener('click',removeNavMobile);
+
+
+
+
+
+
+
+
+
+
